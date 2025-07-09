38d641810d9a1cf1106a9b9eac5899d6ace59433
Surve <surve8729@gmail.com>
2025-07-10 01:17:56 +0530

    Controller and Routes

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Gallery;
+use Illuminate\Support\Facades\Storage;
 
 class GalleryController extends Controller
 {
-    //
+    public function index()
+    {
+        $galleries = Gallery::all();
+        return view('gallery.index', compact('galleries'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GalleryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
+Route::get('/gallery/create', [GalleryController::class, 'create'])->name('gallery.create');
+Route::post('/gallery', [GalleryController::class, 'store'])->name('gallery.store');
+Route::get('/gallery/{id}/edit', [GalleryController::class, 'edit'])->name('gallery.edit');
+Route::put('/gallery/{id}', [GalleryController::class, 'update'])->name('gallery.index');
+Route::delete('/gallery/{id}', [GalleryController::class, 'destroy'])->name('gallery.destroy');
